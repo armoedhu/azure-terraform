ae0680ae4b7295c351d3a3491b65d2f22c4b53a6
Edgar Humberto Arjona Morales <edgara@visionsoftware.com.co>
2025-12-29 15:46:32 -0500

    Add resource group Terraform module

---
+++ modules/resource-group/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  description = "Nombre del Resource Group"
+  type        = string
+}
+
+variable "location" {
+  description = "Región de Azure"
+  type        = string
+}
